6b31032a2e24b1b578833c57051e0ce59288d9e5
Jose Marco Nufio Garrido <jose.m.nufio.0@gmail.com>
2023-04-15 14:22:27 +0200

    Batman

---
+++ src/links.ts
@@ -0,0 +1,15 @@
+
+export const links = {
+	template: `
+<h1>My Links Page</h1>
+<div>
+	<ul>
+		<li><a target="_blank" href="https://twitter.com/nglr_JM">Blue Bird App</a></li>
+		<li><a target="_blank" href="https://youtube.com/@nglr_JM">Red Play Button App</a></li>
+		<li><a target="_blank" href="https://github.com/jmnuf">Hub of Git App</a></li>
+	</ul>
+</div>
+	`,
+};
+
+export type LinksPageComponent = typeof links;
---
+++ src/main.ts
@@ -0,0 +1,75 @@
+import './style.css'
+import { UI } from "@peasy-lib/peasy-ui";
+import { Norishre } from "@jmnuf/norishre";
+import type { LinksPageComponent } from './links';
+
+const home = {
+	template: `<h1>Home page</h1>`,
+};
+const about = {
+	template: `<h1>About Me page</h1>`,
+};
+
+const pages = {
+	home, about,
+	links: null as unknown as LinksPageComponent,
+};
+
+const ranger = new Norishre({
+	home: {
+		loaded: true,
+		path: "/",
+		model: home
+	},
+	about: {
+		loaded: true,
+		path: "/about",
+		model: about,
+	},
+	links: {
+		loaded: false,
+		path: "/links",
+		load: async () => {
+			const script = await import("./links");
+			pages.links = script.links;
+			return script.links;
+		}
+	}
+});
+
+const base_page = ranger.find_arrow_id_by_url();
+
+if (!(base_page in ranger.models) && base_page !== "%404%") {
+	const time_id = `Loading page ${base_page}`;
+	console.time(time_id)
+	await ranger.pull_from_quiver(base_page);
+	let loaded = (base_page in ranger.models);
+	while (!loaded) {
+		await new Promise(res => setTimeout(res, 10));
+		console.log(base_page, ranger.models);
+		loaded = (base_page in ranger.models);
+	}
+	console.timeEnd(time_id);
+}
+
+const nav_links = [
+	ranger.new_link("home"),
+	ranger.new_link("about"),
+	ranger.new_link("links"),
+];
+
+console.log(nav_links);
+
+const app = {
+	nav_links,
+	ranger,
+};
+
+const app_template = document.querySelector("#app-template")!;
+
+UI.create(document.body, app_template, app);
+
+app_template.remove();
+
+// @ts-ignore
+window.norishre = ranger;
